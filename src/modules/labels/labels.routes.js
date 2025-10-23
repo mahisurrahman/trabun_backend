148a5878113360cf653e42ffcbfa6af486f658d7
mahisurrahman <zordanblog009@gmail.com>
2025-10-23 13:01:40 +0600

    LAbel integrated

--- src/modules/labels/labels.routes.js
+++ src/modules/labels/labels.routes.js
@@ -2,21 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const labelController = require("./labels.controller");
-const labelValidator = require("./labels.validator");
-const { authMiddleware } = require("../../middlewares/authMiddleware");
 
-router.post(
-  "/create",
-  labelValidator.createLabel,
-  labelController.createLabelCntrlr
-);
+router.post("/create", labelController.createLabelCntrlr);
 router.get("/getAll", labelController.getAllLabelsCntrlr);
 router.get("/getById/:id", labelController.getLabelByIdCntrlr);
-router.post(
-  "/update/:id",
-  labelValidator.updateLabel,
-  labelController.updateLabelCntrlr
-);
+router.post("/update/:id", labelController.updateLabelCntrlr);
 router.get("/remove/:id", labelController.removeLabelCntrlr);
 router.get("/delete/:id", labelController.hardDeleteLabelCntrlr);
 
